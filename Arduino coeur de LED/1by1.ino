0b537dfeef6df570b7a5b93a53be01ff65054514
Nathanaël Aigret <33094967+TheNatanx@users.noreply.github.com>
2017-11-16 09:30:26 +0100

    Arduino Codes

---
+++ Arduino coeur de LED/1by1.ino
@@ -0,0 +1,20 @@
+const short fDelay = 666;//==>90 BPM
+
+void setup()
+{
+    for(short i=2; i<12; i++) {pinMode(i, OUTPUT);}
+}
+
+void loop()
+{
+  for(short i=2; i<12; i++)
+  {
+    digitalWrite(i-1, LOW);
+    digitalWrite(i, HIGH);
+    delay(fDelay);
+    if(i == 11)
+    {
+      digitalWrite(i, LOW);
+    }
+  }
+}
